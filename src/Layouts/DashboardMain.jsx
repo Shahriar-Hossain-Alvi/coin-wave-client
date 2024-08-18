174ab249c4a753e59491d90c62e700c88751cad1
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-08-19 01:05:18 +0600

    added scroll restoration

--- src/Layouts/DashboardMain.jsx
+++ src/Layouts/DashboardMain.jsx
@@ -1,9 +1,10 @@
 import Sidebar from '../components/Shared/Sidebar/Sidebar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 
 const DashboardMain = () => {
     return (
         <div className="font-raleway flex gap-1 ">
+            <ScrollRestoration />
             <Sidebar></Sidebar>
             <div className="flex-1">
                 <Outlet></Outlet>
